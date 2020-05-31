55b5df69bc36a61226c07e67fc33e74397a3522e
David Novotny <davidnovotny47@gmail.com>
2020-05-31 11:56:03 +0200

    1st commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { HomePageComponent } from './home-page/home-page.component';
+import { ShopingCartComponent } from './shoping-cart/shoping-cart.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router'; // CLI imports router
+
+const routes: Routes = [
+  { path: 'shoping-cart', component: ShopingCartComponent },
+  { path: 'home-page', component: HomePageComponent }
+]; // sets up routes constant where you define your routes
+
+// configures NgModule imports and exports
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ShopingCartComponent } from './shoping-cart/shoping-cart.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      NavBarComponent,
+      ShopingCartComponent,
+      HomePageComponent
+   ],
+   imports: [
+      BrowserModule,
+      NgbModule,
+      AppRoutingModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
-export class AppModule { }
+export class AppModule {}
